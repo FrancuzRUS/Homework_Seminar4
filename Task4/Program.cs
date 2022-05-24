f1e3a82cb3b198a91c34b50caaebfba3a44b0d7f
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-24 12:13:37 +0300

    Task4 modified

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// 4. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 
-int[] mass = new int[10];
+int[] mass = new int[123];
 
 int i = 0;
 int count = 0;
@@ -8,9 +8,9 @@ int lenght = mass.Length;
 
 while (i < lenght)
 {
-    mass[i] = new Random().Next(0,123);
+    mass[i] = new Random().Next(0,100);
     Console.WriteLine(mass[i]);
-    if (mass[i] > 10 && mass[i] < 99) count = count + 1;
+    if (mass[i] > 9 && mass[i] < 100) count = count + 1;
     i++;
 }
 Console.WriteLine($"Количество элементов из отрезка [10,99] = {count}");
